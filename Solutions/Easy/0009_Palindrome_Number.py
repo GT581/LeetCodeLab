f7bc811a0bb246ee039e9de2bc0c117db42ef561
Gerry <gjt581dev@gmail.com>
2025-04-10 03:23:24 -0400

    9: Palindrome Number
    Problem / Instructions

---
+++ Solutions/Easy/0009_Palindrome_Number.py
@@ -0,0 +1,21 @@
+'''
+#9: Palindrome Number
+
+Difficulty:
+Easy
+
+Problem:
+Given an integer `x`, return `true` if `x` is a palindrome, and `false` otherwise.
+
+Instructions:
+- A palindrome is a number that reads the same forward and backward.
+- For example, 121, 1331, and 12321 are palindromes.
+'''
+
+
+
+'''
+Explanation:
+
+
+''' 
