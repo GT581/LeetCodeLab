1495ec4d2c08b16a2dab3039720ffac636d42ab6
Gerry <gjt581dev@gmail.com>
2025-04-13 17:08:56 -0400

    21 Merge Two Sorted Lists

---
+++ Solutions/Easy/0021_Merge_Two_Sorted_Lists.py
@@ -0,0 +1,20 @@
+'''
+#21: Merge Two Sorted Lists
+
+Difficulty:
+Easy
+
+Problem:
+Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Instructions:
+- Both l1 and l2 are sorted in non-decreasing order.
+- The returned list should also be sorted in non-decreasing order.
+'''
+
+
+
+'''
+Explanation:
+
+''' 
