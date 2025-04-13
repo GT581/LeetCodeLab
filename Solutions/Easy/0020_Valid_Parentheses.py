69ba792b38d86f82a5fa6a87c03f066bf7fe68c8
Gerry <gjt581dev@gmail.com>
2025-04-12 22:46:54 -0400

    20 Valid Parentheses

---
+++ Solutions/Easy/0020_Valid_Parentheses.py
@@ -0,0 +1,22 @@
+'''
+#20: Valid Parentheses
+
+Difficulty:
+Easy
+
+Problem:
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+Instructions:
+An input string is valid if:
+  1. Open brackets must be closed by the same type of brackets.
+  2. Open brackets must be closed in the correct order.
+  3. Every close bracket has a corresponding open bracket of the same type.
+'''
+
+
+
+'''
+Explanation:
+
+''' 
